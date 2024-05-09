7f77e209c9a970a380149e410910dc7d23e330d3
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-05-09 14:09:49 +0200

    Enable scheduling

--- src/main/java/no/jonasandersen/premier/ValorantPremierBotApplication.java
+++ src/main/java/no/jonasandersen/premier/ValorantPremierBotApplication.java
@@ -3,9 +3,11 @@ package no.jonasandersen.premier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(PremierProperties.class)
+@EnableScheduling
 public class ValorantPremierBotApplication {
 
   public static void main(String[] args) {
